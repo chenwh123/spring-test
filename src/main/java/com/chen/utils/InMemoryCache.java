689a04d557ccf39b673ff7e25ed1cbaf428f0d6f
chenwh3 <chenwh3@haday.cn>
2023-10-10 08:22:04 +0800

    增加内存缓存工具

--- src/main/java/com/chen/utils/InMemoryCache.java
+++ src/main/java/com/chen/utils/InMemoryCache.java
@@ -2,40 +2,50 @@ package com.chen.utils;
 
 
 import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
 
+/**
+ * @author chenwh3
+ */
+@ToString
+@Slf4j
 public class InMemoryCache<K,V> {
 
     private final Map<K, CacheItem<V>> cache = new ConcurrentHashMap<>();
 
     private final long defaultExpirationTimeMillis;
 
-    private final long extensionTimeMillis;
 
     private final int maxEntries;
 
-    public InMemoryCache(long defaultExpirationTimeMillis, long extensionTimeMillis, int maxEntries) {
+    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
+
+
+    public InMemoryCache(long defaultExpirationTimeMillis, int maxEntries) {
         this.defaultExpirationTimeMillis = defaultExpirationTimeMillis;
-        this.extensionTimeMillis = extensionTimeMillis;
         this.maxEntries = maxEntries;
+        SCHEDULER.scheduleAtFixedRate(this::sweep, 30, 30, TimeUnit.MINUTES);
     }
 
     // Default: 1 minute expiration time, extend by 30 seconds on access
     public InMemoryCache() {
-        this(60 * 1000, 30 * 1000, 100);
+        this(60 * 1000,  100);
     }
 
     public void put(K key, V value, long expirationTimeMillis) {
         if (cache.size() >= maxEntries) {
             sweep();
         }
-        cache.put(key, new CacheItem<>(value, System.currentTimeMillis() + expirationTimeMillis));
+        cache.put(key, new CacheItem<>(value,  expirationTimeMillis));
     }
 
     public void put(K key, V value) {
@@ -45,7 +55,7 @@ public class InMemoryCache<K,V> {
     public Object get(K key) {
         CacheItem<V> item = cache.get(key);
         if (item != null && item.isValid()) {
-            item.extendExpirationTime(extensionTimeMillis);
+            item.extend();
             return item.getValue();
         } else {
             remove(key);
@@ -59,36 +69,50 @@ public class InMemoryCache<K,V> {
     }
 
     public synchronized void sweep() {
-        if (cache.size() < maxEntries) {
-            return;
-        }
-        long currentTimeMillis = System.currentTimeMillis();
+        log.info("开始清理内存缓存 , 缓存数量 = {}", cache.size());
         List<K> keysToRemove = new ArrayList<>();
-        for (K key : cache.keySet()) {
-            CacheItem<V> item = cache.get(key);
-            if (!item.isValid() || currentTimeMillis >= item.getExpirationTimeMillis()) {
+        for (Map.Entry<K, CacheItem<V>> entry : cache.entrySet()) {
+            K key = entry.getKey();
+            CacheItem<V> item = entry.getValue();
+            if (!item.isValid()) {
                 keysToRemove.add(key);
             }
         }
         for (K keyToRemove : keysToRemove) {
             cache.remove(keyToRemove);
         }
+        if (cache.size() >= maxEntries) {
+            cache.clear();
+        }
+        log.info("清理内存缓存完成 , 缓存数量 = {}", cache.size());
     }
 
     public void remove(K key) {
         cache.remove(key);
     }
 
+    public Map<K, CacheItem<V>> peek(){
+        return Collections.unmodifiableMap(cache);
+    }
+
+    public void clear(){
+        cache.clear();
+    }
+
     @Data
+    @ToString
     private static class CacheItem<V> {
 
         private final SoftReference<V> reference;
 
         private long expirationTimeMillis;
 
+        private long deadLine;
+
         public CacheItem(V value , long expirationTimeMillis) {
             this.reference = new SoftReference<>(value);
             this.expirationTimeMillis = expirationTimeMillis;
+            this.deadLine = System.currentTimeMillis() + expirationTimeMillis;
         }
 
         public V getValue() {
@@ -96,11 +120,16 @@ public class InMemoryCache<K,V> {
         }
 
         public boolean isValid() {
-            return System.currentTimeMillis() < expirationTimeMillis && reference.get() != null;
+            return System.currentTimeMillis() < deadLine && reference.get() != null;
         }
 
+        public synchronized void extend() {
+            deadLine = System.currentTimeMillis() + expirationTimeMillis;;
+        }
+
+
         public synchronized void extendExpirationTime(long extensionTimeMillis) {
-            expirationTimeMillis += extensionTimeMillis;
+            deadLine += extensionTimeMillis;
         }
     }
 
