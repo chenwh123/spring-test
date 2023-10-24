684ed669955acb6e3cf1b33d145715eb4341afb2
chenwh3 <chenwh3@haday.cn>
2023-10-24 18:04:03 +0800

    add tree utils

--- src/main/java/com/chen/tool/redis/MySemaphore.java
+++ src/main/java/com/chen/tool/redis/MySemaphore.java
@@ -1,25 +1,80 @@
 package com.chen.tool.redis;
 
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.*;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 
+/**
+ * @author chenwh3
+ */
 @Slf4j
 public class MySemaphore {
 
-    private Queue<CompletableFuture<Void>> waitQueue = new ConcurrentLinkedQueue<>();
-
-    private AtomicInteger i = new AtomicInteger();
-
-    public Map<Long, Integer> threadResourceMap = new ConcurrentHashMap<>();
+    private AtomicInteger permits = new AtomicInteger();
 
     MySemaphore(int num) {
-        i.set(num);
+        permits.set(num);
+    }
+
+    private static class Node {
+        private Thread thread;
+        private volatile Node next;
+        private volatile Node prev;
+
+        Node(Thread thread) {
+            this.thread = thread;
+        }
+
+
+    }
+
+    @Data
+    private static class List{
+
+        private  AtomicReference<Node> head;
+        private  AtomicReference<Node> tail;
+
+        public  List init(){
+            List list = new List();
+            list.head = new AtomicReference<>();
+            list.tail = new AtomicReference<>();
+            Node node = new Node(null);
+            list.head.set(node);
+            list.tail.set(node);
+            return list;
+        }
+
+        /**
+         * use cas
+         */
+        public void add(Node node){
+            Node tailNode = tail.get();
+            while (true) {
+                if (tail.compareAndSet(tailNode, node)) {
+                    tailNode.next = node;
+                    node.prev = tailNode;
+                    break;
+                }
+            }
+        }
+
+        public void removeHead(){
+            Node headNode = head.get();
+            while (true) {
+                if (head.compareAndSet(headNode, headNode.next)) {
+                    headNode.next.prev = null;
+                    headNode.next = null;
+                    break;
+                }
+            }
+        }
+
     }
 
     /**
@@ -60,6 +115,7 @@ public class MySemaphore {
         Semaphore semaphore = new Semaphore(1);
         for (int i = 0; i < 10; i++) {
             semaphore.acquire(11);
+
             semaphore.release(1);
         }
     }
