f10b3daf7451b73e42435b2245cb4563c4287953
chenwh3 <chenwh3@haday.cn>
2024-04-17 16:26:03 +0800

    手写ThreadLocal.md

---
+++ src/main/java/com/chen/tool/juejin/project/difficult/Test.java
@@ -0,0 +1,34 @@
+package com.chen.tool.juejin.project.difficult;
+
+public class Test {
+
+    /**
+     * 1.前端配置化开发 , 表单，列表，导入导出等
+     * 2.后端通用查询，入参转sql , 自定义分页 , 分页优化 ;
+     *  - #{}预编译防止sql注入
+     *  - SystemMetaObject 提取sql
+     * 3.Sap连接工具，解决数组转对象问题
+     * 4.Feign工具
+     *  - spel（动态运行）定义数据字段，错误标识字段，错误信息字段。
+     *  - 支持附带token, session, 各种自定义参数
+     *
+     *  业务难题
+     *  - 物料-订单聚合查询
+     *  - 订单-特性组-特性-费用查询/修改
+     *  - 分页优化 ，
+     *  - 前端报表
+     *  - 批量插入优化
+     *  - 物料+工厂+订单时间范围-建立留样单
+     *  - 埋点-MDC问题， decorator
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("H");
+    }
+}
